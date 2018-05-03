bb761d7e74b336a73072a87be69ba662c789e941
Callous Nguyen <nguyenmanhhung9b@gmail.com>
2018-05-03 20:32:54 +0700

    Creating TODOs

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,8 +1,7 @@
 //Ckech off Specific TODOLIST by Clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
 	$(this).toggleClass('completed');
 });
-
 //Click on X to delete TODO
 $("span").click(function(event) {
 	/* Act on the event */
@@ -10,4 +9,14 @@ $("span").click(function(event) {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	/* Act on the event */
+	if(event .which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		//Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " +todoText+ "</li>");
+	}
 });
