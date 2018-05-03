f3eee2f2c9fb19909a817264eae85133775733cb
Callous Nguyen <nguyenmanhhung9b@gmail.com>
2018-05-03 19:38:00 +0700

    Delete TODO

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,12 @@
 $("li").click(function () {
 	$(this).toggleClass('completed');
 });
+
+//Click on X to delete TODO
+$("span").click(function(event) {
+	/* Act on the event */
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
